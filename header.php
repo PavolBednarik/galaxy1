b221595407466aee3d0e02b95e51d6ca4a2b997e
PavolBednarik <152331129+PavolBednarik@users.noreply.github.com>
2024-04-08 17:08:43 +0100

    project

---
+++ header.php
@@ -0,0 +1,124 @@
+<?php
+
+require_once ("connection.php");
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
+    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    
+  <style>
+    /* styling for poster */
+    .object-fit-cover {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      object-position: center;
+    }
+
+    /* styling for footer */
+    .blog-footer {
+      padding: 2.5rem 0;
+      color: #fbf9f9;
+      text-align: center;
+      background-color: black;
+      border-top: 0.05rem solid #e5e5e5;
+    }
+
+    /* movie template styling */
+    #movie-template {
+      margin-bottom: 50px;
+    }
+
+    body {
+      height: 100%;
+      background-color: #060516c1;
+    }
+
+    /* Autocomplete dropdown styling */
+    .autocomplete-items {
+      position: absolute;
+      border: 1px solid #d4d4d4;
+      border-radius: 4px;
+      max-height: 200px;
+      overflow-y: auto;
+      z-index: 99;
+      background-color: white;
+      top: 54px;
+    }
+
+    /* autocomlete list item styling */
+    .autocomplete-item {
+      padding: 5px 10px;
+      cursor: pointer;
+    }
+
+    .autocomplete-item:hover {
+      background-color: #e9e9e9;
+    }
+  </style>
+
+  <title>Galaxy Cinema</title>
+</head>
+<!-- navigation bar from Bootstrap adjusted for webside need -->
+
+<body>
+  <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Galaxy Cinema</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="navbar-brand" href="GalaxyCinema.html">Home</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link btn dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+            Cinemas
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown" style="padding: 0.5rem 1rem;">
+            <a class="dropdown-item" href="Cinema-Coolock.html">Galaxy Coolock</a>
+            <a class="dropdown-item" href="Cinema-Rathmines.html">Galaxy Rathmines</a>
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link btn dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+            Movies
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown" style="padding: 0.5rem 1rem;">
+            <a class="dropdown-item" href="allMovies.html">Available movies</a>
+            <a class="dropdown-item" href="upcomingMovies.html">Comming soon</a>
+          </div>
+        </li>
+        <!-- login, sign in  and search button-->
+
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" id="search-field">
+        <div id="autocomplete-dropdown" class="autocomplete-items" style="display: none;"></div>
+
+        <button class="btn btn-outline btn-secondary my-2 my-sm-0" id="search-btn" type="submit">Search</button>
+      </form>
+      <div class="d-flex justify-content-end">
+        <a href="Login.html" class="btn btn-primary ">Login</a>
+        <a href="Singin.html" class="btn btn-danger">Sign In</a>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container">
---
+++ movie-detail.php
@@ -0,0 +1,145 @@
+<?php
+
+require_once ("header.php");
+
+$sql = "SELECT 
+m.movie_id,
+m.title,
+m.synopsis,
+m.release_date,
+m.runtime,
+m.poster,
+m.picture1,
+m.picture2,
+m.picture3,
+m.youtube_id,
+GROUP_CONCAT(DISTINCT g.name ORDER BY g.name SEPARATOR ', ') AS genres,
+GROUP_CONCAT(DISTINCT a.name ORDER BY a.name SEPARATOR ', ') AS actors
+FROM 
+movies m
+LEFT JOIN movie_genre mg ON m.movie_id = mg.movies_id
+LEFT JOIN genres g ON mg.genre_id = g.genre_id
+LEFT JOIN actor_movie am ON m.movie_id = am.movie_id
+LEFT JOIN actors a ON am.actor_id = a.actor_id
+GROUP BY 
+m.movie_id
+ORDER BY 
+m.movie_id;";
+$result = $conn->query($sql);
+$all_movies = array(); // Initialize an empty array to store all movies
+while($row = $result->fetch_assoc()) {
+    $all_movies[] = $row; // Append each movie row to the array
+}
+$all_movies_json = json_encode($all_movies);
+//echo $all_movies_json;
+
+?>
+ <div class="row">
+      <div class="col-md-5">
+        <a href="#" id="movie-poster-link" data-lightbox="gallery">
+          <img src="#" id="movie-poster" alt="Movie poster" class="img-thumbnail object-fit-cover">
+        </a>
+      </div>
+      <div class="col-md-7">
+        <h3 class="text-light text-end" id="movie-title"></h3>
+        <p class="text-light text-end">
+          <span class="badge bg-secondary" id="movie-genre"></span>
+          <span class="badge bg-secondary" id="movie-year"></span>
+        </p>
+        <p class="text-light text-end" id="movie-synopsis">
+        </p>
+        <p class="text-light text-end">
+          <span class="badge bg-secondary" id="movie-time"></span>
+          <span class="badge bg-secondary" id="movie-actor"></span>
+        </p>
+        <p class="text-light text-end">Available at:
+          <span class="badge bg-secondary" id="movie-cinema"></span>
+        </p>
+        <div class="d-flex align-items-center">
+          <a href="booking.html" class="btn btn-primary">Book now</a>
+        </div>
+      </div>
+
+    </div>
+  </div>
+  </div>
+  </div>
+  <br>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 offset-md-3">
+        <div class="ratio ratio-16x9 mw-100">
+          <iframe id="movie-video" class="mw-100" width="560" height="315"
+            src="https://www.youtube.com/embed/U2Qp5pL3ovA?si=_Bz9Zy2-MpQIArWz" title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen></iframe>
+        </div>
+      </div>
+    </div>
+  </div>
+  <br>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <a href="#" id="movie-gallery-link-1" data-lightbox="gallery">
+          <img src="#" id="movie-gallery-1" alt="Movie poster" class="img-thumbnail object-fit-cover">
+        </a>
+      </div>
+      <div class="col-md-4">
+        <a href="#" id="movie-gallery-link-2" data-lightbox="gallery">
+          <img src="#" id="movie-gallery-2" alt="Movie poster" class="img-thumbnail object-fit-cover">
+        </a>
+      </div>
+      <div class="col-md-4">
+        <a href="#" id="movie-gallery-link-3" data-lightbox="gallery">
+          <img src="#" id="movie-gallery-3" alt="Movie poster" class="img-thumbnail object-fit-cover">
+        </a>
+      </div>
+    </div>
+  </div>
+  <br>
+  <script>
+    
+  var urlParams = new URLSearchParams(window.location.search);
+    var movieId = parseInt(urlParams.get('movie-id'));
+
+    let data = <?php $all_movies_json; ?>;
+    // Fetch movie data from 'movies1.json'
+    fetch('http://localhost/web%20development/movie-detail.php?movie-id=' + movieId)
+      .then(response => response.json())
+      .then(data => {
+        // Find the movie with the matching ID
+        const movie = data.find(m => m.movie_id === movieId);
+        console.log(movie);
+        if (movie) {
+          // Populate various elements on the page with movie details
+          document.getElementById('movie-title').textContent = movie.title;
+          document.getElementById('movie-synopsis').textContent = movie.synopsis;
+          document.getElementById('movie-actor').textContent = `Actor: ${movie.star}`;
+          document.getElementById('movie-genre').textContent = `Genre: ${movie.genres.join(', ')}`;
+          document.getElementById('movie-year').textContent = `Year: ${movie.year}`;
+          document.getElementById('movie-time').textContent = `${movie.runtime} minutes`;
+          document.getElementById('movie-poster-link').setAttribute("href", movie.poster);
+          document.getElementById('movie-poster').setAttribute("src", movie.poster);
+          document.getElementById('movie-video').setAttribute("src", "https://www.youtube.com/embed/" + movie.video.youtube_id);
+          document.getElementById('movie-gallery-link-1').setAttribute("href", movie.picture1);
+          document.getElementById('movie-gallery-1').setAttribute("src", movie.picture1);
+          document.getElementById('movie-gallery-link-2').setAttribute("href", movie.picture2);
+          document.getElementById('movie-gallery-2').setAttribute("src", movie.picture2);
+          document.getElementById('movie-gallery-link-3').setAttribute("href", movie.picture3);
+          document.getElementById('movie-gallery-3').setAttribute("src", movie.picture3);
+        }
+
+      })
+      .catch(error => {
+        // Handle errors if the fetch operation fails
+        console.log("moMovie id " + movieId + " wasn't found");
+      }
+      );
+
+  <?php
+
+require_once ("footer.php");
+
+?>
